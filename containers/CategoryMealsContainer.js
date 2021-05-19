19b3c4762b9fe944332077b6d7c6847188a58517
ah-james <11ahj11@gmail.com>
2021-05-19 15:15:37 -0400

    Basic Container Components Started

---
+++ containers/CategoryMealsContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const CategoryMealsContainer = props => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default CategoryMealsContainer
---
+++ containers/MealContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+
+const MealContainer = props => {
+    return (
+        <View>
+            
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default MealContainer
