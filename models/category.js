8a6a3ede570e0d1047f3071df2e7f68a0015b55e
ah-james <11ahj11@gmail.com>
2021-05-20 21:33:12 -0400

    Adding Category Dummy Data

---
+++ models/category.js
@@ -0,0 +1,10 @@
+class Category {
+    // initialize object on class w/ constructor
+    constructor(id, title, color) {
+        this.id = id
+        this.title = title
+        this.color = color
+    }
+}
+
+export default Category
