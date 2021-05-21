c0b3ed1e5de5055fb2c018b129ff638b9c81fbff
ah-james <11ahj11@gmail.com>
2021-05-20 21:53:27 -0400

    Adding Colors to Categories Container Header

--- containers/CategoriesContainer.js
+++ containers/CategoriesContainer.js
@@ -1,21 +1,38 @@
 import React from 'react'
-import { View, Text, StyleSheet, FlatList } from 'react-native'
+import { View, Text, StyleSheet, FlatList, TouchableOpacity, Platform } from 'react-native'
 import { CATEGORIES } from '../data/dummy-data'
+import Colors from '../constants/Colors'
 
-// create variable to use in renderItem for FlatList
-const renderGridItem = itemData => {
-    return(
-        <View style={styles.gridItem}>
-            {/* place title from CATEGORIES data in Text tag */}
-            <Text>{itemData.item.title}</Text>
-        </View>
-    )
-}
+const CategoriesContainer = props => {
+    // create variable to use in renderItem for FlatList
+    const renderGridItem = itemData => {
+        return(
+            // make each grid item touchable, linked to the CategoryMeals container
+            <TouchableOpacity onPress={() => {
+                props.navigation.navigate({ routeName: 'CategoryMeals' })
+            }} >
+                <View style={styles.gridItem}>
+                    {/* place title from CATEGORIES data in Text tag */}
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        )
+    }
 
-const FoodCategoriesContainer = props => {
     return (
         <FlatList numColumns={2} data={CATEGORIES} renderItem={renderGridItem} />
     )
+}
+
+// CategoriesContainer is JS object, so can add property after creation
+CategoriesContainer.navigationOptions = {
+    // Add title to header
+    headerTitle: 'Meal Categories',
+    // style header
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+    },
+    headerTintColor: Platform.OS === 'ios' ? Colors.primaryColor : 'black'
 }
 
 const styles = StyleSheet.create({
@@ -31,4 +48,4 @@ const styles = StyleSheet.create({
     },
 })
 
-export default FoodCategoriesContainer
+export default CategoriesContainer
