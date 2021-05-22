e69c324f52e902cf599080bd6c76e2af3874cbc6
ah-james <11ahj11@gmail.com>
2021-05-21 23:22:47 -0400

    Adding Favorite Star Icon to Meal Show Page

--- containers/MealContainer.js
+++ containers/MealContainer.js
@@ -1,15 +1,41 @@
 import React from 'react'
 import { View, Text, StyleSheet, Button } from 'react-native'
+import { MEALS } from '../data/dummy-data'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import HeaderButton from '../components/HeaderButton'
 
 const MealContainer = props => {
+    const mealId = props.navigation.getParam('mealId')
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
+
     return (
         <View style={styles.screen}>
-            <Text>Look At One Meal</Text>
+            <Text>{selectedMeal.title}</Text>
             <Button title="Home" onPress={() => {
                 props.navigation.popToTop()
             }} />
         </View>
     )
+}
+
+MealContainer.navigationOptions = navigationData => {
+    const mealId = navigationData.navigation.getParam('mealId')
+    const selectedMeal = MEALS.find(meal => meal.id === mealId)
+
+    return{
+        headerTitle: selectedMeal.title,
+        headerRight: () => <HeaderButtons HeaderButtonComponent={HeaderButton}>
+            {/* use Item to describe Icon you want to show */}
+            <Item 
+                title='Favorite' 
+                iconName='ios-star-outline' 
+                onPress={() => {
+                    console.log('mark as favorite')
+                }
+            } />
+        </HeaderButtons>
+    }
+
 }
 
 const styles = StyleSheet.create({
