4b4428c3a5e500ed8af31dd78f7fd83c57feb5a6
ah-james <11ahj11@gmail.com>
2021-05-22 01:11:07 -0400

    Adding Favorites Navigator

--- navigation/MealsNavigator.js
+++ navigation/MealsNavigator.js
@@ -11,6 +11,17 @@ import FavoritesContainer from '../containers/FavoritesContainer'
 import { Ionicons } from '@expo/vector-icons'
 import { createMaterialBottomTabNavigator } from 'react-navigation-material-bottom-tabs'
 
+const defaultNavOptions = {
+    // second argument in createStackNavigator allows you to configure the navigator
+    // defaultNavigationOptions sets up options applied to every screen
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
+        },
+        headerTintColor: Platform.OS === 'ios' ? Colors.primaryColor : 'black',
+    }
+}
+
 const MealsNavigator = createStackNavigator({
     // first argument in createStackNavigator is object of screens
     Categories: {
@@ -20,15 +31,17 @@ const MealsNavigator = createStackNavigator({
         screen: CategoryMealsContainer,
     },
     MealDetail: MealContainer,
-}, {
-    // second argument in createStackNavigator allows you to configure the navigator
-    // defaultNavigationOptions sets up options applied to every screen
-    defaultNavigationOptions: {
-        headerStyle: {
-            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : '',
-        },
-        headerTintColor: Platform.OS === 'ios' ? Colors.primaryColor : 'black',
+    }, 
+    {
+        defaultNavigationOptions: defaultNavOptions
     }
+)
+
+const FavoritesNavigator = createStackNavigator({
+    Favorites: FavoritesContainer,
+    MealDetail: MealContainer,
+}, {
+    defaultNavigationOptions: defaultNavOptions
 })
 
 const tabScreenConfig = {
@@ -40,7 +53,7 @@ const tabScreenConfig = {
         }
         
     },
-        Favorites: {screen: FavoritesContainer, navigationOptions: {
+        Favorites: {screen: FavoritesNavigator, navigationOptions: {
             tabBarIcon: (tabInfo) => {
                 return <Ionicons name='ios-star' size={22} color={tabInfo.tintColor} />
             },
