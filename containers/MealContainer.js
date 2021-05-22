d3d907cb9d5b4efaa5213d12325aef3df6971683
ah-james <11ahj11@gmail.com>
2021-05-22 14:50:19 -0400

    Building Out Meal Page

--- containers/MealContainer.js
+++ containers/MealContainer.js
@@ -1,21 +1,39 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
 
 import { MEALS } from '../data/dummy-data'
 import HeaderButton from '../components/HeaderButton'
+import DefaultText from '../components/DefaultText'
+
+const ListItem = props => {
+    return(
+        <View style={styles.listItem}>
+            <DefaultText>{props.children}</DefaultText>
+        </View>
+    )
+}
 
 const MealContainer = props => {
     const mealId = props.navigation.getParam('mealId')
     const selectedMeal = MEALS.find(meal => meal.id === mealId)
 
     return (
-        <View style={styles.screen}>
-            <Text>{selectedMeal.title}</Text>
-            <Button title="Home" onPress={() => {
-                props.navigation.popToTop()
-            }} />
-        </View>
+        <ScrollView>
+            <Image source={{uri: selectedMeal.imageUrl}} style={styles.image} />
+            <View style={styles.detail}>
+                <DefaultText>{selectedMeal.affordability}</DefaultText>
+                <DefaultText>{selectedMeal.complexity}</DefaultText>
+                <DefaultText>{selectedMeal.duration} Minutes</DefaultText>
+            </View>
+            <Text style={styles.title}>Ingredients</Text>
+            {selectedMeal.ingredients.map(ingredient => 
+                <ListItem key={ingredient}>{ingredient}</ListItem>
+            )}
+            <Text style={styles.title}>Instructions</Text>
+            {selectedMeal.steps.map(step => 
+                <ListItem key={step}>{step}</ListItem>)}
+        </ScrollView>
     )
 }
 
@@ -40,11 +58,27 @@ MealContainer.navigationOptions = navigationData => {
 }
 
 const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
+    image: {
+        width: '100%',
+        height: 200,
     },
+    detail: {
+        flexDirection: 'row',
+        padding: 15,
+        justifyContent: 'space-around',
+    },
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 20,
+        textAlign: 'center',
+    },
+    listItem: {
+        marginVertical: 10,
+        marginHorizontal: 20,
+        borderColor: '#ccc',
+        borderWidth: 1,
+        padding: 10, 
+    }
 })
 
 export default MealContainer
