6788b5bc4c69060367700039e9088623f2880b28
ah-james <11ahj11@gmail.com>
2021-05-22 01:11:28 -0400

    Adding List to Favorites Page

--- containers/FavoritesContainer.js
+++ containers/FavoritesContainer.js
@@ -1,20 +1,14 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import MealList from '../components/MealList'
+import { MEALS } from '../data/dummy-data'
 
 const FavoritesContainer = props => {
-    return (
-        <View style={styles.screen}>
-            <Text>Your Favorites</Text>
-        </View>
-    )
+    const favoriteMeals = MEALS.filter(meal => meal.id === 'm1' || meal.id === 'm2')
+    return <MealList filteredMeals={favoriteMeals} navigation={props.navigation} />
 }
 
-const styles = StyleSheet.create({
-    screen: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
-})
+FavoritesContainer.navigationOptions = {
+    headerTitle: 'Your Favorites'
+}
 
 export default FavoritesContainer
